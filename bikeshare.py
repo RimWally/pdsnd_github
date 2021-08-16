c57c42404b3757658d1059b599d4d47202413055
rim <reemalaa514@gmail.com>
2021-08-16 19:01:53 +0200

    edits to  bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -6,3 +6,12 @@
     df.drop('day_of_week',axis=1,inplace=True)
     df.drop('month',axis=1,inplace=True)
     return df
+@@ -153,6 +154,8 @@ def time_stats(df):
+    print('\nCalculating The Most Frequent Times of Travel...\n')
+    start_time = time.time()
+
+    df['day_of_week'] = pd.to_datetime(df['Start Time']).dt.dayofweek
+    df['month'] = pd.to_datetime(df['Start Time']).dt.month
+    #temporary_df = pd.read_csv(city)
+    # TO DO: display the most common month
+    most_freq_month = df['month'].mode()[0]
